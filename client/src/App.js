8e5193b3b9b6e03a0c27680d2fc6dd2a05a8f94f
fwilbor <franklin.cfg@gmail.com>
2019-10-12 11:35:18 -0400

    update images and configure components

--- client/src/App.js
+++ client/src/App.js
@@ -128,7 +128,7 @@ class App extends Component {
           ))}
         </GridMDC>
         <BottomNavMDC style={{ background: "#313133", marginTop: "17.5px", paddingTop: "15px", borderTop: "2.5px solid slategray" }}>
-          <a href="https://github.com/philiptd5000/clicky-game-REACT" target="_blank" className="link" alt="clicky-game-github-link"><i className="fa fa-github fa-2x"></i></a>
+          <a href="" target="_blank" className="link" alt="clicky-game-github-link"><i className="fa fa-github fa-2x"></i></a>
         </BottomNavMDC>
 
       </div>
--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Card.css";
-// import { url } from "inspector";
-import {inspect, report} from 'import-inspector';
+
 
 const Card = ({ id, name, image, handlePicked }) => (
     // <div className={`mdc-layout-grid__cell--span-3--order-${Number(id)}`}>
